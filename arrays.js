aa2b5bd213e38075abbb64e0faf173646144eb62
Zachary Jeffreys <zachary.jeffreys@wsu.edu>
2019-01-24 22:53:10 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,12 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+function addElementToBeginningOfArray(array, element){
+ 
+  const newArray = array
+  newArray.unshift(element);
+  return newArray;
+}
+ function destructivelyAddElementToBeginningOfArray(array, element){
+   array.unshift(element);
+   return array
+ }
